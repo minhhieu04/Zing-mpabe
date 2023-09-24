f3b52530df5ce66b9dad4d299a4b817a1ba5dfde
minhhieu04 <minhhieu.tran.mcs@gmail.com>
2023-09-24 22:59:14 +0700

    Coding searchArtist Page v1 have bug

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import icons from "../utils/icons";
 import * as actions from "../store/actions";
 import { useDispatch } from "react-redux";
-import { useNavigate, createSearchParams } from "react-router-dom";
+import { useNavigate, createSearchParams, useParams } from "react-router-dom";
 import path from "../utils/path";
 import {AiOutlineClose} from "react-icons/ai"
 
@@ -11,6 +11,7 @@ const { BsSearchHeart } = icons;
 const Search = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const { singer } = useParams()
 
   const [keyword, setKeyword] = useState("");
 
@@ -29,13 +30,13 @@ const Search = () => {
   return (
     <div className="flex items-center relative text-gray-500">
       <span onClick={()=> setKeyword('')} className="absolute right-[16px] cursor-pointer"><AiOutlineClose size={22}/></span>
-      <span className="flex h-10 pl-4 bg-main-200 justify-center items-center rounded-l-[20px]">
+      <span className={`flex h-10 pl-4 ${singer ? 'bg-[rgba(0,0,0,0.2)]' : 'bg-main-200' } justify-center items-center rounded-l-[20px]`}>
         <BsSearchHeart size={24} />
       </span>
       <input
         type="text"
         placeholder="Tìm kiếm bài hát, nghệ sĩ, lời bài hát..."
-        className="outline-none bg-main-200 px-4 py-2 rounded-r-[20px] h-10 w-full"
+        className={`outline-none ${singer ? 'bg-[rgba(0,0,0,0.2)]' : 'bg-main-200' } px-4 py-2 rounded-r-[20px] h-10 w-full`}
         value={keyword}
         onChange={(e) => setKeyword(e.target.value)}
         onKeyUp={handleSearch}
--- src/containers/public/Public.js
+++ src/containers/public/Public.js
@@ -20,7 +20,7 @@ const Public = () => {
               <LoadingSpinner />
             </div>
           )}
-          <div className="h-[70px] flex-none px-[59px] flex items-center">
+          <div className="h-[70px] fixed top-0 left-[240px] right-[329px] z-50 px-[59px] flex items-center">
             <Header />
           </div>
           <div className="w-full flex-auto">
