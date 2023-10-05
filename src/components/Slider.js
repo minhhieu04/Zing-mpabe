37763173c11213dfa57b47f652568bd3e8f04be6
Emma minhhieu04 <minhhieu.tran.mcs@gmail.com>
2023-10-05 14:01:29 +0700

    Fix bug Slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,75 +1,70 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
 import { getArrSlider } from "../utils/fn";
 import * as actions from "../store/actions";
+import { useNavigate } from "react-router-dom";
+
 const Slider = () => {
   const { banner } = useSelector((state) => state.app);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  //animation for slider
+  // ainimation for banner
   useEffect(() => {
     const sliderEls = document.getElementsByClassName("slider-item");
-    let min = 1;
-    let max = 3;
+    let min = 0;
+    let max = 2;
     const intervalId = setInterval(() => {
       const list = getArrSlider(min, max, sliderEls.length - 1);
-
       for (let i = 0; i < sliderEls.length; i++) {
+        // Delete classnames (css)
         sliderEls[i]?.classList?.remove(
           "animate-slide-right",
           "order-last",
-          "z-10"
+          "z-20"
         );
         sliderEls[i]?.classList?.remove(
           "animate-slide-left",
           "order-first",
-          "z-0"
+          "z-10"
         );
         sliderEls[i]?.classList?.remove(
           "animate-slide-left2",
           "order-2",
-          "z-0"
+          "z-10"
         );
 
+        // Hide or Show images
         if (list.some((item) => item === i)) {
           sliderEls[i].style.cssText = `display: block`;
         } else {
           sliderEls[i].style.cssText = `display: none`;
         }
       }
+      // Add animation by adding classnames
       list.forEach((item) => {
         if (item === max) {
-          sliderEls[max]?.classList?.add(
+          sliderEls[item]?.classList?.add(
             "animate-slide-right",
             "order-last",
-            "z-10"
+            "z-20"
           );
         } else if (item === min) {
-          sliderEls[min]?.classList?.add(
+          sliderEls[item]?.classList?.add(
             "animate-slide-left",
             "order-first",
-            "z-0"
+            "z-10"
           );
         } else {
           sliderEls[item]?.classList?.add(
             "animate-slide-left2",
             "order-2",
-            "z-0"
+            "z-10"
           );
         }
       });
-      if (min === sliderEls.length - 1) {
-        min = 0;
-      } else {
-        min += 1;
-      }
-      if (max === sliderEls.length - 1) {
-        max = 0;
-      } else {
-        max += 1;
-      }
+      min = min === sliderEls.length - 1 ? 0 : min + 1;
+      max = max === sliderEls.length - 1 ? 0 : max + 1;
     }, 3000);
     return () => {
       intervalId && clearInterval(intervalId);
@@ -81,9 +76,7 @@ const Slider = () => {
       dispatch(actions.setCurSongId(item.encodeId));
       dispatch(actions.play(true));
       dispatch(actions.setPlaylist(null));
-    }
-    if (item?.type === 4) {
-      // console.log(item);
+    } else if (item?.type === 4) {
       const albumPath = item?.link?.split(".")[0];
       navigate(albumPath);
     } else {
@@ -92,17 +85,19 @@ const Slider = () => {
   };
 
   return (
-    <div className="flex gap-8 w-full overflow-hidden px-[59px] pt-8">
-      {banner?.map((item, index) => (
-        <img
-          key={item.encodeId}
-          src={item.banner}
-          onClick={() => handleClickBanner(item)}
-          className={`slider-item flex-1 object-contain w-[30%] rounded-lg ${
-            index <= 2 ? "block" : "hidden"
-          }`}
-        />
-      ))}
+    <div className="w-full overflow-hidden px-[59px]">
+      <div className="flex w-full gap-8 pt-8">
+        {banner?.map((item, index) => (
+          <img
+            key={item.encodeId}
+            src={item.banner}
+            onClick={() => handleClickBanner(item)}
+            className={`slider-item flex-1 object-contain w-[30%] rounded-lg ${
+              index <= 2 ? "block" : "hidden"
+            }`}
+          />
+        ))}
+      </div>
     </div>
   );
 };
