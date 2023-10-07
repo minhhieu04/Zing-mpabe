a09ce9f47a2768f6210d4cbf78c7407d4bac37a7
minhhieu04 <99400715+minhhieu04@users.noreply.github.com>
2023-10-07 23:57:28 +0700

    Add animation loading spinner when reload web

--- src/containers/public/Home.js
+++ src/containers/public/Home.js
@@ -8,6 +8,7 @@ import {
 } from "../../components";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { LoadingSpinner } from "../../components";
 
 const Home = () => {
   const {
@@ -20,34 +21,48 @@ const Home = () => {
     top100,
   } = useSelector((state) => state.app);
   return (
-    <div className="overflow-y-auto w-full">
-      <div className="w-full h-[70px]"></div>
-      <Slider />
-      <NewRelease />
-      <Section data={section1} />
-      <Section data={section2} />
-      <Section data={section3} />
-      <Section data={section4} />
-      <Section data={section5} />
-      <ChartSection />
-      <div className="w-full flex items-center px-[43px] mt-12">
-        {weekChart?.map((item) => (
-          <Link
-            to={item.link.split(".")[0]}
-            key={item.link}
-            className="flex-1 px-4"
-          >
-            <img
-              src={item.cover}
-              alt="cover"
-              className="w-full object-cover rounded-md"
-            />
-          </Link>
-        ))}
-      </div>
-      <Top100 data={top100} />
-      <div className="w-full h-[500px]"></div>
-    </div>
+    <>
+      {section1 &&
+      section2 &&
+      section3 &&
+      section4 &&
+      section5 &&
+      weekChart &&
+      top100 ? (
+        <div className="overflow-y-auto w-full">
+          <div className="w-full h-[70px]"></div>
+          <Slider />
+          <NewRelease />
+          <Section data={section1} />
+          <Section data={section2} />
+          <Section data={section3} />
+          <Section data={section4} />
+          <Section data={section5} />
+          <ChartSection />
+          <div className="w-full flex items-center px-[43px] mt-12">
+            {weekChart?.map((item) => (
+              <Link
+                to={item.link.split(".")[0]}
+                key={item.link}
+                className="flex-1 px-4"
+              >
+                <img
+                  src={item.cover}
+                  alt="cover"
+                  className="w-full object-cover rounded-md"
+                />
+              </Link>
+            ))}
+          </div>
+          <Top100 data={top100} />
+          <div className="w-full h-[500px]"></div>
+        </div>
+      ) : (
+        <div className="w-full h-full flex items-center justify-center">
+          <LoadingSpinner />
+        </div>
+      )}
+    </>
   );
 };
 
